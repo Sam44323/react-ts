4ec9e8631002eb38e44b425d6c7c41531f2ac395
Suranjan Dutta Moitra <suranjanbiswas2002@gmail.com>
2021-03-30 21:34:11 +0530

    Added the styles for the NewTodo component

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -2,6 +2,8 @@ import React, {useRef} from 'react';
 
 import {AddTodo} from '../todo.model'
 
+import styles from './styles/NewTodo.module.css';
+
 interface NewTodoProps{
   addTodo: AddTodo
 }
@@ -14,14 +16,14 @@ const NewTodo: React.FC<NewTodoProps> = (props) => {
     props.addTodo(enteredText)
   }
 
-  return <form onSubmit={todoSubmitHandler }>
+  return <form onSubmit={todoSubmitHandler } className={styles.formSection}>
     <div>
-      <label htmlFor="todo-text">Todo Text</label>
+      <label htmlFor="todo-text" className={styles.inputTitle}>Todo Text</label>
       <br/>
-      <input type="text" name="todo-text" ref={textInputRef}/>
+      <input type="text" name="todo-text" ref={textInputRef} className={styles.inputField}/>
     </div>
     <br/>
-    <button type="submit">ADD TODO</button>
+    <button type="submit" className={styles.formButton}>ADD TODO</button>
   </form>
 }
 
