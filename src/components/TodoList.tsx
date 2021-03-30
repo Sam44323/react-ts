f9f22a23b2d5bf5e97c4dee87dd47b2db701950f
Suranjan Dutta Moitra <suranjanbiswas2002@gmail.com>
2021-03-30 21:55:38 +0530

    Refactoring

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import {Todo, DeleteMethod} from '../todo.model'
+import { Todo, DeleteMethod } from "../todo.model";
 
-import styles from './styles/TodoList.module.css';
+import styles from "./styles/TodoList.module.css";
 
-interface TodoListProps{
-  items: Todo[],
-  deleteTodo: DeleteMethod
+interface TodoListProps {
+  items: Todo[];
+  deleteTodo: DeleteMethod;
 }
 
-const TodoList: React.FC <TodoListProps> = (props) => {
-  return <ul className={styles.uoList}>
-    {props.items.map(item => 
-    <li key={item.id} className={styles.listItem}>
-      <span className={styles.listText}>{item.text}</span>
-      <br/>
-      <button onClick={()=> props.deleteTodo(item.id)} className={styles.listButton}>Delete</button>
-      </li>)}
-  </ul>
-}
+const TodoList: React.FC<TodoListProps> = (props) => {
+  return (
+    <ul className={styles.uoList}>
+      {props.items.map((item) => (
+        <li key={item.id} className={styles.listItem}>
+          <span className={styles.listText}>{item.text}</span>
+          <br />
+          <button
+            onClick={() => props.deleteTodo(item.id)}
+            className={styles.listButton}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default TodoList
+export default TodoList;
